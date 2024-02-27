f47956e57bed89024c7a74d30c85c54c64b4cdff
shivimishra <shivimishra130103@gmail.com>
2024-02-27 17:46:43 +0530

    framer motion

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -4,30 +4,30 @@ import { fetchAnime } from "@/app/action";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
-import AnimeCard, { AnimeProp } from "./AnimeCard";
+import AnimeCard from "./AnimeCard";
 
 //pages need to update , else with each scroll it'll continue displaying the same content, i.e. page 2 in this case
 let page = 2;
 
+export type AnimeCard = JSX.Element;
+
 function LoadMore() {
   const { ref, inView } = useInView();        //hooks can only be used on client side
-  const [data, setData] = useState<AnimeProp[]>([]);
+  const [data, setData] = useState<AnimeCard[]>([]);
 
   useEffect(() => {
     if(inView) {
       fetchAnime(page).then((res) => {
-        setData([...data,...res])           //...data cuz we wish to retain the previous data as well
-        page++;         //increment the page with each data loading
+        setData([...data,...res])             //...data cuz we wish to retain the previous data as well
+        page++;                              //increment the page with each data loading
       });
     }
-  }, [inView, data]);                   //div ref={ref} , when ref is reached this will execute
+  }, [inView, data]);                         //div ref={ref} , when ref is reached this will execute
   
   return (
     <>
       <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
-        {data.map((item: AnimeProp, index: number) => (
-          <AnimeCard key={item.id} anime={item} index={index} />
-        ))}
+        {data}
       </section>
       <section className="flex justify-center items-center w-full">
         <div ref={ref}>         
---
+++ components/MotionDiv.tsx
@@ -0,0 +1,6 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+export const MotionDiv = motion.div;
+//now this is the only client component and we can keep it separate and use it in animecard, as it has server components 
